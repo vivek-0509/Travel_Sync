e7892227af4f5079945dc8966c7f8d9941078190
Vivek <vivek@Viveks-MacBook-Air.local>
2025-09-28 18:03:14 +0530

    Impleneted the routes for user

--- internal/user/routes/userRoutes.go
+++ internal/user/routes/userRoutes.go
@@ -1,1 +1,21 @@
 package routes
+
+import (
+	handler "Travel_Sync/internal/user/hander"
+
+	"github.com/gin-gonic/gin"
+)
+
+func RegisterUserRoutes(router *gin.Engine, userHandler *handler.UserHandler) {
+	api := router.Group("/api")
+	{
+		user := api.Group("/user")
+		{
+			user.POST("", userHandler.CreateUser)
+			user.DELETE("/:id", userHandler.DeleteUser)
+			user.PUT("/:id", userHandler.UpdateUser)
+			user.GET("/:id", userHandler.GetUserById)
+			user.GET("", userHandler.GetAllUser)
+		}
+	}
+}
