414bfe44fbddedc890630daa9699a14fa1dc1233
vivek-0509 <viveksolanki0509@gmail.com>
2025-10-01 15:00:05 +0530

    made chnages in user route to apply rate limiting

--- internal/user/routes/userRoutes.go
+++ internal/user/routes/userRoutes.go
@@ -1,6 +1,7 @@
 package routes
 
 import (
+	"Travel_Sync/internal/middleware"
 	"Travel_Sync/internal/security/config"
 	"Travel_Sync/internal/security/service"
 	handler "Travel_Sync/internal/user/hander"
@@ -14,6 +15,8 @@ func RegisterUserRoutes(router *gin.Engine, userHandler *handler.UserHandler, jw
 		user := api.Group("/user")
 		// Apply JWT middleware to all user routes
 		user.Use(config.JWTMiddleware(jwtService))
+		// Apply rate limiting to user routes
+		user.Use(middleware.GeneralRateLimiter())
 		{
 			//user.POST("", userHandler.CreateUser)
 			user.DELETE("/:id", userHandler.DeleteUser)
