cbf29d7d796ab7570bcf44510c0a185cd7f5a81c
vivek-0509 <viveksolanki0509@gmail.com>
2025-10-01 14:49:01 +0530

    Implemented travel recommendation models for travel ticket

--- internal/travel/models/travelRecommendation.go
+++ internal/travel/models/travelRecommendation.go
@@ -1,1 +1,16 @@
 package models
+
+import tentity "Travel_Sync/internal/travel/entity"
+
+type ScoredTicket struct {
+	Ticket tentity.TravelTicket `json:"ticket"`
+	Score  float64              `json:"score"`
+	Date   string               `json:"date"`
+	Time   string               `json:"time"`
+}
+
+type RecommendationResult struct {
+	BestMatch         *ScoredTicket  `json:"best_match"`
+	BestGroup         []ScoredTicket `json:"best_group"`
+	OtherAlternatives []ScoredTicket `json:"other_alternatives"`
+}
