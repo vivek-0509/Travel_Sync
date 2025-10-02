1fe347005a3a473e77a7a9d8594cc3a6a4660f23
vivek-0509 <viveksolanki0509@gmail.com>
2025-10-02 14:15:05 +0530

    Changes the redirect url

--- internal/security/authConfig/authConfig.go
+++ internal/security/authConfig/authConfig.go
@@ -11,7 +11,7 @@ func GetGoogleOAuthConfig() *oauth2.Config {
 	return &oauth2.Config{
 		ClientID:     os.Getenv("GOOGLE_CLIENT_ID"),
 		ClientSecret: os.Getenv("GOOGLE_CLIENT_SECRET"),
-		RedirectURL:  "http://localhost:8080/auth/google/callback",
+		RedirectURL:  "https://travel-sync-0ank.onrender.com/auth/google/callback",
 		Scopes:       []string{"email", "profile"},
 		Endpoint:     google.Endpoint,
 	}
